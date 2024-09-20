28680cfcffd336f77a01df664c27cbe5f523f56f
Jp-moraiss <164018039+Jp-moraiss@users.noreply.github.com>
2024-09-19 23:38:45 -0300

    URL atualizada
    URL \admin adicionada e 2 SuperUser criados

--- cuida/cuida/urls.py
+++ cuida/cuida/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from app_cuida import views
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.home,name='home'),
+    path('admin/', admin.site.urls),
 ]
