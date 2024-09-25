2fc4035a6813b48cc0e5b107720d338034ced494
Henrique Figueiredo <hft@cesar.school>
2024-09-25 04:17:58 -0300

    organização de codigo e progressao na auth

--- cuida/app_cuida/views.py
+++ cuida/app_cuida/views.py
@@ -5,8 +5,36 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib import messages
 
-def form(request):
 
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['nome']
+        email = request.POST['email']
+        password = request.POST['senha']
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        messages.success(request, 'Conta criada com sucesso! Faça login.')
+        return redirect('login')
+
+    return render(request, 'accounts/login.html') 
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['nome']
+        password = request.POST['senha']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home') 
+        else:
+            messages.error(request, 'Nome de usuário ou senha inválidos.')
+
+    return render(request, 'accounts/login.html')
+
+def form(request):
     if request.method == 'GET':
         pacientes = Paciente.objects.all()
         form = PacienteForm()
@@ -31,7 +59,6 @@ def form(request):
                 'form': form,
             }
             return render(request, 'app_cuida/form.html', context)
-
 
 def update(request, id_paciente):
     if request.method == 'GET':
@@ -81,37 +108,7 @@ def pacientes(request):
 def delete_paciente(request, id_paciente):
     paciente = get_object_or_404(Paciente, id_paciente=id_paciente)
     paciente.delete()
-    return redirect('editar_pacientes')
-
-def register(request):
-    if request.method == 'POST':
-        username = request.POST['nome']
-        email = request.POST['email']
-        password = request.POST['senha']
-
-        user = User.objects.create_user(username=username, email=email, password=password)
-        user.save()
-        
-
-        messages.success(request, 'Conta criada com sucesso! Faça login.')
-        return redirect('login')
-
-    return render(request, 'app_cuida/login.html') 
-
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['nome']
-        password = request.POST['senha']
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home') 
-        else:
-            messages.error(request, 'Nome de usuário ou senha inválidos.')
-
-    return render(request, 'app_cuida/login.html')  
-
+    return redirect('editar_pacientes')  
 
 def edit(request):
     pacientes = Paciente.objects.all()
