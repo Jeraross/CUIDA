7f2a73959f41c966891ced04f94291f86665f2a7
Maria Júlia Dantas 👩‍💻 <mariajulia.dantas.08@gmail.com>
2024-09-25 14:21:54 -0300

    Revert "accounts added in settings.py"
    This reverts commit a6745f9fd3bd6910d4f79ff224773d1ada1911e1.

--- cuida/cuida/settings.py
+++ cuida/cuida/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_cuida',
-    'accounts',
+    'app_cuida'
 ]
 
 MIDDLEWARE = [
