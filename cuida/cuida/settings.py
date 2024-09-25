00dca8e5ae318dc594a7fe96fd8ea25f6631554b
Henrique Figueiredo <hft@cesar.school>
2024-09-25 02:54:52 -0300

    criação da aplicação accounts
    começo da autenticação do usuario

--- cuida/cuida/settings.py
+++ cuida/cuida/settings.py
@@ -131,3 +131,4 @@ STATIC_ROOT= os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
