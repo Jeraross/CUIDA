8f11d09a9ff1417eb93015aa2f5164e6f266094a
Maria Júlia Dantas 👩‍💻 <mariajulia.dantas.08@gmail.com>
2024-09-25 20:19:03 -0300

    ajuste staticfiles
    corrigindo erro do css e cache

--- cuida/cuida/settings.py
+++ cuida/cuida/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT= os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 
 # Default primary key field type
