a6745f9fd3bd6910d4f79ff224773d1ada1911e1
Henrique Figueiredo <hft@cesar.school>
2024-09-25 02:56:45 -0300

    accounts added in settings.py

--- cuida/cuida/settings.py
+++ cuida/cuida/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_cuida'
+    'app_cuida',
+    'accounts',
 ]
 
 MIDDLEWARE = [
